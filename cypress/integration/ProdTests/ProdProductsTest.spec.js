9212f4250c3acc28eace36c3d6d4784dc68cbf86
Bruno Faria <bruno.faria@inventa.shop>
2022-05-09 18:36:06 -0300

    Adding more test cases for products in production

--- cypress/integration/ProdTests/ProdProductsTest.spec.js
+++ cypress/integration/ProdTests/ProdProductsTest.spec.js
@@ -1,13 +1,42 @@
 /// <reference types="cypress" />
 
 describe('Testing search feature', () => {
-  it('Will search for shampoo transparente and expect to get just one label', () => {
+
+  Cypress.on(
+    'uncaught:exception',
+    (_,__) =>
+      false,
+  );
+
+  before(() => {
     cy.visit('https://inventa.shop/collections/promocoes-1');
+  })
+  
+  it("Will search for shampoo transparente and expect to don't get a product", () => {
 
     cy.get('.modal-closer').first().click();
     cy.get('.search-bar-button').click();
     cy.get('.search-bar__input-wrapper').first().type('Shampoo transparente{enter}');
     cy.contains('Ver preço')
-    cy.contains('Shampoo claro e transparente').should('have.length', 0);
+    cy.contains('a','Shampoo claro e transparente').should('have.length', 0);
   })
+
+  it("Will search for Perfume transparente and expect to don't get a product", () => {
+
+    cy.get('.search-bar-button').click();
+    cy.get('.search-bar__input-wrapper').type('Perfume transparente{enter}');
+    cy.get('.modal-closer').first().click();
+    cy.contains('a','Perfume transparente').should('have.length', 0);
+  })
+
+  it("Will search for Batom líquido transparente and expect to don't get a product ", () => {
+
+
+    cy.get('.search-bar-button').click();
+    cy.get('.search-bar__input-wrapper').type('Batom líquido transparente{enter}');
+    cy.get('.modal-closer').first().click();
+    cy.contains('Ver preço')
+    cy.contains('a','Batom líquido transparente').should('have.length', 0);
+  })
+  
 });
