5dad660f429c40d1fc12d7448263a4cbc03953ff
Caíque Coelho <caique.coelho@inventa.shop>
2022-06-14 19:27:02 -0300

    refactoring feedback error

--- cypress/integration/ProdTests/RetailerRegistration.spec.js
+++ cypress/integration/ProdTests/RetailerRegistration.spec.js
@@ -26,18 +26,30 @@ describe('Register', () => {
             registerPage.alert.containsVisibleText('Forneça um número de telefone com DDD válido')
         })
 
-        it('CPF, CNPJ, Email and password without filling I need to see a feedback error', function () {
+        it('CPF, CNPJ, Email and Password without filling I need to see a feedback error', function () {
             registerPage.registerFormRetailer(this.retailerDataComplete)
             registerPage.clickButtonNext()
             registerPage.clickButtonSubmit()
 
             registerPage.alert.containsVisibleText('Adicione o seu CPF')
             registerPage.alert.containsVisibleText('CPF é inválido, favor validar os números informados')
+
+            registerPage.getCPFInput(this.retailerWithShortPassword.cpf)
+            registerPage.clickButtonSubmit()
             registerPage.alert.containsVisibleText('Adicione o seu CNPJ')
             registerPage.alert.containsVisibleText('CNPJ é inválido, favor validar os números informados')
+
+            registerPage.getCNPJInput(this.retailerWithShortPassword.cnpj)
+            registerPage.clickButtonSubmit()
             registerPage.alert.containsVisibleText('Insira um e-mail')
+
+            registerPage.getEmailInput(this.retailerWithShortPassword.email)
+            registerPage.clickButtonSubmit()
             registerPage.alert.containsVisibleText('Insira uma senha')
             registerPage.alert.containsVisibleText('Insira uma senha com mais de 6 caracteres')
+
+            registerPage.getPasswordInput(this.retailerWithShortPassword.password)
+            registerPage.clickButtonSubmit()
             registerPage.alert.containsVisibleText('Necessário que aceite os termos e condições')
         })
     })
@@ -46,14 +58,16 @@ describe('Register', () => {
         const registerPage = new RegisterPage();
         const passwords = ['1', '12345']
 
-        it(`Must not register with passwords: ${passwords}`, function () {
+        it.only(`Must not register with passwords: ${passwords}`, function () {
             registerPage.registerFormRetailer(this.retailerDataComplete)
             registerPage.clickButtonNext()
-            registerPage.clickTermsAndConditions();
+
+            registerPage.getTemsCheckbox().should('be.visible');
+            registerPage.registerFormRetailerDetailsWithouPassword(this.retailerWithShortPassword);
+            registerPage.getTemsCheckbox().click();
 
             passwords.forEach((password) => {
-                this.retailerWithShortPassword.password = password;
-                registerPage.registerFormRetailerDetails(this.retailerWithShortPassword);
+                registerPage.getPasswordInput().type(password);
                 registerPage.clickButtonSubmit();
             });
         });
--- cypress/support/Pages/RegisterPage/RegisterPage.js
+++ cypress/support/Pages/RegisterPage/RegisterPage.js
@@ -7,9 +7,25 @@ class RegisterPage {
         this.alert = alert
     }
 
+    getCPFInput = () => cy.get(el.CPF_BY_ID);
+
+    getCNPJInput = () => cy.get(el.CNPJ_BY_ID);
+
+    getEmailInput = () => cy.get(el.EMAIL_BY_ID);
+
+    getPasswordInput = () => cy.get(el.PASSWORD_BY_ID);
+
+    getTemsCheckbox = () => cy.get(el.TERMS_AND_CONDITIONS_BY_ID)
+
     registerFormRetailer(user) {
         cy.get(el.NAME_BY_ID).type(user.name)
         cy.get(el.PHONE_BY_ID).type(user.phone)
+    }
+
+    registerFormRetailerDetailsWithouPassword(user) {
+        cy.get(el.CPF_BY_ID).clear().type(user.cpf)
+        cy.get(el.CNPJ_BY_ID).clear().type(user.cnpj)
+        cy.get(el.EMAIL_BY_ID).clear().type(user.email)
     }
 
     registerFormRetailerDetails(user) {
